21b754587254a38c96b74746b8162b9e3a8c1375
Luís Henrique <lhmaruyama@gmail.com>
2023-01-29 22:21:33 -0300

    test new

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from './components/header/header'
+import Body from './components/body/body'
+import Footer from './components/footer/footer'
+import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+
+        <div className='header-app'>
+          <Header/>
+        </div>
+
+        <div className='body-app'>
+          <Body/>
+        </div>
+
+        <div className='footer-app'>
+          <Footer/>
+        </div>
+
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
